55464e5633ad236614da1702460dd8518b9aecf7
Aleksey Vorobev <pinochetlab@gmail.com>
2023-10-05 17:59:42 +0300

    made homework 2 but it doesn't work(

--- main.py
+++ main.py
@@ -1,34 +1,64 @@
-"""This module for FastAPI."""
-from fastapi import FastAPI
+from fastapi import APIRouter, FastAPI
+from pydantic import BaseModel
+
+
+class d(BaseModel):
+    def __init__(self, m: dict[str, float], *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.m = m;
+
+class Expr:
+    def evaluate(self, values):
+        raise NotImplementedError("Please Implement this method")
+
+
+class Param(Expr):
+    def __init__(self, name):
+        self.name = name
+
+    def evaluate(self, values):
+        return values[self.name]
+
+
+class Value(Expr):
+    def __init__(self, val):
+        self.val = val
+
+    def evaluate(self, values):
+        return self.val
+
+
+class MyMath:
+    def __init__(self):
+        self.myRouter = APIRouter()
+        self.setup_routes()
+        self.expressions = []
+
+    def setup_routes(self):
+        @self.myRouter.get("/")
+        async def root():
+            return {"message": "Hello World"}
+
+        @self.myRouter.get("/items/{item_id}")
+        async def read_item(item_id: int, q: str = None):
+            return {"item_id": item_id, "q": q}
+
+        @self.myRouter.get("/params")
+        async def params():
+            return map(lambda x: Param(x).name, filter(lambda x: isinstance(x, Param), self.expressions))
+
+        @self.myRouter.post("/add_val_{e}")
+        async def add_val(e):
+            self.expressions.append(Value(e))
+
+        @self.myRouter.post("/add_par_{e}")
+        async def add_val(e):
+            self.expressions.append(Param(e))
+
+        @self.myRouter.post("/eval")
+        async def evaluate(m: d):
+            return sum(map(lambda x: x.evaluate(m), self.expressions))
+
 
 app = FastAPI()
-
-
-@app.get("/")
-async def root1():
-    """Default GET request."""
-    return {"message": "Hello World"}
-
-
-@app.get("/123")
-async def root2():
-    """GET request returning [1, 2, 3]."""
-    return [1, 2, 3]
-
-
-@app.get("/123concat{s}")
-async def root3(numbers: str):
-    """GET request with query parameter returning "123" and s concatenation."""
-    return "123" + numbers
-
-
-@app.post("/123concat")
-async def root4(item: list[int]):
-    """Post request returning concatenation of [1, 2, 3] and list in request body."""
-    return [1, 2, 3] + item
-
-
-@app.post("/concat{s}")
-async def root5(numbers: str, item: list[int]):
-    """Post request returning concatenation of (list[int])s and list in request body."""
-    return list(map(int, numbers)) + item
+app.include_router(MyMath().myRouter)
